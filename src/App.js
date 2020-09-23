5dc972deafcd0cbd5f7883095e34ad368e9c51d9
Hongli Wang <1023836798@qq.com>
2020-09-23 00:39:55 -0400

    FInish first edition of alanNews

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState }from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './component/NewsCards/NewsCards';
+import useStyles from './styles.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = '3b6bda96db4f8238aceb4492bc52a7d42e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () =>{
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command , articles}) =>{
+                if(command === 'newHeadlines'){
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                }else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                }
+            }
+        })
+    }, [])
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                <img className={classes.alanLogo}src="https://res.cloudinary.com/springboard-images/image/upload/q_auto,f_auto,fl_lossy/wordpress/2018/08/Blog-AI.png" alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    );
 }
-
 export default App;
---
+++ src/component/NewsCard/NewsCard.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect, createRef} from 'react'
+import {Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import useStyles from './styles.js'
+import classNames from 'classnames';
+
+const NewsCard = ({article:{ description, publishedAt, source, title, url, urlToImage}, i, activeArticle }) => {
+    const classes = useStyles();
+    const [elRefs, setElrefs] = useState([]);
+    const scrollToRef = (ref) => window.scroll(0,ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        setElrefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if(i === activeArticle && elRefs[activeArticle]){
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs]);
+
+    return (
+        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || 'https://st.depositphotos.com/1152339/1972/i/950/depositphotos_19723583-stock-photo-news-concept-news-on-digital.jpg'}/>
+                <div className={classes.details}>
+                    <Typography variant="body2" color = "textSecondary" component='h2'>{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color = "textSecondary" component='h2'>{source.name}</Typography>
+                </div>
+                <Typography className={classes.title}gutterBottom variant="h5">{title}</Typography>
+                <CardContent>
+                    <Typography variant='body2' color="textSecondary" component='p'>{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
+            </CardActions>
+        </Card>
+    )
+}
+export default NewsCard;
